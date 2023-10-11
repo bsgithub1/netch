a19d00941150fa91d7f08d80db96ba3a386f85ec
saurabh.patil <saurabh.pa@browserstack.com>
2023-10-11 16:46:05 +0530

    "PR-V2Ray : Update SN core to 5.0.16"

--- Other/v2ray-sn/build.ps1
+++ Other/v2ray-sn/build.ps1
@@ -23,7 +23,6 @@ $Env:GOROOT_FINAL='/usr'
 
 $Env:GOOS='windows'
 $Env:GOARCH='amd64'
-go get -u ./...
-go mod tidy
+go mod tidy # necessary
 go build -a -trimpath -asmflags '-s -w' -ldflags '-s -w -buildid=' -o '..\..\release\v2ray-sn.exe' '.\main'
 exit $lastExitCode
